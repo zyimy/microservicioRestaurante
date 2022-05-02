01bbf1d1eb4a9c40256b5d32b8d55c3435bd5e2c
Yimy Zepeda <zepedayimy45@gmail.com>
2022-05-02 23:07:22 +0200

    w

--- restaurante/src/main/java/com/proyecto/restaurante/controller/RestauranteController.java
+++ restaurante/src/main/java/com/proyecto/restaurante/controller/RestauranteController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @CrossOrigin("*")
-@RequestMapping("/v1/restaurante")
+@RequestMapping("/restaurante")
 public class RestauranteController {
 
     @Autowired
--- restaurante/src/main/java/com/proyecto/restaurante/feignClients/PackFeign.java
+++ restaurante/src/main/java/com/proyecto/restaurante/feignClients/PackFeign.java
@@ -6,7 +6,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 //, url = "http://localhost:8002/v1/pack"
-@FeignClient(name = "pack", url = "http://localhost:8002/v1/pack")
+
+@FeignClient(name = "pack")
 public interface PackFeign {
 
     @GetMapping("/allPack/{idRestaurante}")
