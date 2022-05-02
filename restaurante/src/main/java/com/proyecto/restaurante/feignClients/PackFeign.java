97b9362ecf292af3815958013988dc3f5882dd67
zyimy <zepedayimy46@gmail.com>
2022-05-02 12:26:07 +0200

    w

--- restaurante/src/main/java/com/proyecto/restaurante/feignClients/PackFeign.java
+++ restaurante/src/main/java/com/proyecto/restaurante/feignClients/PackFeign.java
@@ -2,10 +2,7 @@ package com.proyecto.restaurante.feignClients;
 
 import com.proyecto.restaurante.modelo.Pack;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
