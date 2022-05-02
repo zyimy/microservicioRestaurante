c4107a090375f7ff0136145905fa8a32b875acbb
zyimy <zepedayimy46@gmail.com>
2022-05-02 11:56:01 +0200

    f

--- pack/src/main/java/com/proyecto/pack/PackApplication.java
+++ pack/src/main/java/com/proyecto/pack/PackApplication.java
@@ -2,8 +2,10 @@ package com.proyecto.pack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PackApplication {
 
 	public static void main(String[] args) {
--- restaurante/src/main/java/com/proyecto/restaurante/RestauranteApplication.java
+++ restaurante/src/main/java/com/proyecto/restaurante/RestauranteApplication.java
@@ -2,10 +2,12 @@ package com.proyecto.restaurante;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableEurekaClient
 public class RestauranteApplication {
 
 	public static void main(String[] args) {
