bb7f16fae9998ba1e2a5af17042bc88bbccb1550
Yimy Zepeda <zepedayimy45@gmail.com>
2022-05-02 22:43:44 +0200

    d

--- restaurante/src/main/java/com/proyecto/restaurante/feignClients/PackFeign.java
+++ restaurante/src/main/java/com/proyecto/restaurante/feignClients/PackFeign.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 //, url = "http://localhost:8002/v1/pack"
-@FeignClient(name = "pack")
+@FeignClient(name = "pack", url = "http://localhost:8002/v1/pack")
 public interface PackFeign {
 
     @GetMapping("/allPack/{idRestaurante}")
